60ff3c6be1e2b66f5ecd0c62a7eb0a0fe5241037
xyvlin <valerie.lin1023@gmail.com>
2025-02-03 17:53:03 +0800

    edit decript shift cypher

--- decriptShiftCypher.cpp
+++ decriptShiftCypher.cpp
@@ -3,7 +3,9 @@ using namespace std;
 using namespace std::chrono;
 vector<int> keys;
 vector<string> words;
+int cc[26];
 
+//split the sentence into a vector of words
 void split(string str, char del){
     string temp = "";
     for(int i=0; i<str.size(); i++){
@@ -17,6 +19,7 @@ void split(string str, char del){
     }  
 }
 
+//shift cipher
 string shift(string plain, int k){
     string cip="";
     for(int i=0;i<plain.size();i++){
@@ -32,6 +35,34 @@ string shift(string plain, int k){
     }
     return cip;
 }
+
+//most occuring letter in message
+int maxOfcc(){
+    int maxx = 0;
+    for(int i=0;i<26;i++){
+        if(cc[i]>cc[maxx]){
+            maxx=i;
+        }
+    }
+    return maxx;
+}
+
+//check if all words have vowels or y
+bool check(int key){
+    for(int j=0;j<words.size();j++){
+        string cur = shift(words[j],key);
+        bool hasVowel=false;
+        for(int k=0;k<cur.size();k++){
+            if(cur[k]=='a' || cur[k]=='e' || cur[k]=='i' || cur[k]=='o' || cur[k]=='u' || cur[k]=='y'){
+                hasVowel=true;
+                break;
+            }
+        }
+        if(!hasVowel) return false;
+    }
+    return true;
+}
+
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(0);
     string message, decrypted;
@@ -40,29 +71,28 @@ int main(){
     split(message,' ');
 
     auto start = high_resolution_clock::now();
-    for(int i=0;i<words.size();i++){
-        if(words[i].size()==1){
-            if('a'<=words[i][0] && words[i][0]<='z'){
-                keys.push_back('i'-words[i][0]);
-                keys.push_back('a'-words[i][0]);
-            }else{
-                keys.push_back('I'-words[i][0]);
-                keys.push_back('A'-words[i][0]);
-            }
-        }
+    for(int i=0;i<message.size();i++){
+        if('a'<=message[i]&&message[i]<='z')
+            cc[message[i]-'a']++;
     }
-    if(!keys.empty()){
-        for(int i=0;i<keys.size();i++){
-            cout<<"key "<<keys[i]<<": ";
-            decrypted=shift(message,keys[i]);
-            cout<<decrypted<<endl;
-        }
+    int maxx = maxOfcc()+'a';
+
+    if(check('e'-maxx)){
+        cout<<"key "<<'e'-maxx<<":　"<<shift(message,'e'-maxx)<<endl;
+    }else if(check('a'-maxx)){
+        cout<<"key "<<'a'-maxx<<":　"<<shift(message,'a'-maxx)<<endl;
+    }else if(check('o'-maxx)){
+        cout<<"key "<<'o'-maxx<<":　"<<shift(message,'o'-maxx)<<endl;
+    }else if(check('i'-maxx)){
+        cout<<"key "<<'i'-maxx<<":　"<<shift(message,'i'-maxx)<<endl;
+    }else if(check('u'-maxx)){
+        cout<<"key "<<'u'-maxx<<":　"<<shift(message,'u'-maxx)<<endl;
     }else{
-        cout<<"I don't know what to do";
+        cout<<"I'm never decripting this message"<<endl;
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
     cout << "time taken: "<<duration.count() << " ms" << endl;
 }
 
-//Ashley's message: d vh cpibmt. don nj xjgy ojyvt.
+//Ashley's message: rcvo vmz rz cvqdib ajm gpixc ojyvt? rcvo yj tjp rvio oj zvo ajm ydiizm?
