869c9d8525565009fd274b946e36c4e62e80fada
xyvlin <111556881+xyvlin@users.noreply.github.com>
2025-03-17 12:36:50 +0800

    Create shiftCipherDemo.py

---
+++ shiftCipherDemo.py
@@ -0,0 +1,45 @@
+# error!!
+def shift_cipher(message, key):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    shifted_alphabet = alphabet[key:] + alphabet[:key]
+    translation_table = {alphabet[i]: shifted_alphabet[i] for i in range(26)}
+    
+    message = message.upper()
+    encrypted_message = "".join(translation_table.get(char, char) for char in message)
+    
+    return encrypted_message, alphabet, shifted_alphabet
+
+def display_shift(alphabet, shifted_alphabet, key):
+    alphabet = alphabet + alphabet[:key]
+    shifted_alphabet = list(str(i) for i in range(1, key+1))+alphabet[:26]
+    for i in range(0,len(alphabet)):
+        print(alphabet[i],shifted_alphabet[i])
+
+def main():
+    print("Shift Cipher works by shifting all the characters in your message by a fixed number of position. For example, move A by 1 position and it becomes B; move A by 2 position it becomes C.")
+    print('The number of positions you shift the letters is called the "key".') 
+    print("This program will encrypt your message using a shift cipher.")
+    
+    message = input("Enter your message: ")
+    while True:
+        try:
+            key = int(input("Enter a shift key (1-25): "))
+            if 1 <= key <= 25:
+                break
+            else:
+                print("Please enter a number between 1 and 25.")
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 25.")
+    
+    encrypted_message, alphabet, shifted_alphabet = shift_cipher(message, key)
+    display_shift(alphabet, shifted_alphabet, key)
+    
+    print("\nEncryption Process:")
+    for original, encrypted in zip(message.upper(), encrypted_message):
+        if original in alphabet:
+            print(f"{original} → {encrypted}")
+    
+    print("\nCiphertext:", encrypted_message)
+
+if __name__ == "__main__":
+    main()
